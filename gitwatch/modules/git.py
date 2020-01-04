8c58bc261193e661b3b407d5cca661ef5253352b
Jan Dvorak <dvoraj75@gmail.com>
2020-01-04 06:34:04 +0100

    Create base structure, add requirements.txt

---
+++ gitwatch/modules/git.py
@@ -0,0 +1,25 @@
+
+
+class GitApi:
+
+    def __init__(self):
+        pass
+
+    def pull(self):
+        pass
+
+    def push(self):
+        pass
+
+    def commit(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remote(self):
+        pass
+
+    def branch(self):
+        pass
+
