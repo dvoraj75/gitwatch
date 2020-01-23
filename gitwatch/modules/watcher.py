d6d4dd67e11f338f6e56324d8118c9eaf836e777
Jan Dvorak <dvoraj75@gmail.com>
2020-01-23 21:02:32 +0100

    Docstrings

--- gitwatch/modules/watcher.py
+++ gitwatch/modules/watcher.py
@@ -3,20 +3,35 @@ from watchdog.events import FileSystemEventHandler
 
 
 class FileWatcher:
-
+    """
+    Simple file system watcher which run Observer for file
+    """
     def __init__(self, path: str):
+        """
+        Args:
+            path(str): path to file or directory
+        """
         self._observer = Observer()
         self.path = path
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Run simple observer for the file
+        """
         event_handler = EventHandler()
         self._observer.schedule(event_handler, self.path, recursive=True)
 
-    def stop(self):
+    def stop(self) -> None:
+        """
+        Stop the observer
+        """
         self._observer.stop()
         self._observer.join()
 
 
 class EventHandler(FileSystemEventHandler):
+    """
+    Simple filesystem event handler
+    """
     def on_any_event(self, event):
         pass
